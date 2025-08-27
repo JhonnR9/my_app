5377b4a0442312a9a5a83c52593c26f6906a619f
jhonnr9 <4.jhones@gmail.com>
2025-08-27 11:13:15 -0300

    feat add some usual libraries

--- src/app.cpp
+++ src/app.cpp
@@ -7,30 +7,20 @@
 #include <algorithm>
 #include "app.h"
 #include <chrono>
+#include <SimpleMath.h>
+#include <PrimitiveBatch.h>
+#include <VertexTypes.h>
+#include <WICTextureLoader.h>
+
+using namespace DirectX;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
-    App *pApp = nullptr;
-
-    if (msg == WM_NCCREATE) {
-        const auto *pCreate = reinterpret_cast<CREATESTRUCT *>(lParam);
-        pApp = static_cast<App *>(pCreate->lpCreateParams);
-        SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pApp));
-    } else {
-        pApp = reinterpret_cast<App *>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
-    }
-
-    if (pApp) {
+    if (App *pApp = App::GetAppFromWndProc(hWnd, msg, lParam)) {
         switch (msg) {
             case WM_SIZE: {
                 UINT w = LOWORD(lParam);
                 UINT h = HIWORD(lParam);
                 pApp->Resize(w, h);
-                break;
-            }
-            case WM_PAINT: {
-                PAINTSTRUCT ps;
-                BeginPaint(hWnd, &ps);
-                EndPaint(hWnd, &ps);
                 break;
             }
             case WM_DESTROY:
@@ -40,7 +30,6 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam) {
             default:
                 return DefWindowProc(hWnd, msg, wParam, lParam);
         }
-
         return 0;
     }
 
@@ -52,46 +41,6 @@ App::App() {
     wcscpy_s(szTitle, L"My App");
     wcscpy_s(szWindowClass, L"D3D11WindowClass");
 }
-
-App::~App() {
-    if (g_pRenderTargetView) {
-        g_pRenderTargetView->Release();
-        g_pRenderTargetView = nullptr;
-    }
-    if (pSwapChain) {
-        pSwapChain->Release();
-        pSwapChain = nullptr;
-    }
-    if (g_pPixelShader) {
-        g_pPixelShader->Release();
-        g_pPixelShader = nullptr;
-    }
-    if (g_pVertexShader) {
-        g_pVertexShader->Release();
-        g_pVertexShader = nullptr;
-    }
-    if (g_pVertexLayout) {
-        g_pVertexLayout->Release();
-        g_pVertexLayout = nullptr;
-    }
-    if (g_pVertexBuffer) {
-        g_pVertexBuffer->Release();
-        g_pVertexBuffer = nullptr;
-    }
-    if (pImmediateContext) {
-        pImmediateContext->Release();
-        pImmediateContext = nullptr;
-    }
-    if (pd3dDevice) {
-        pd3dDevice->Release();
-        pd3dDevice = nullptr;
-    }
-    if (g_pIndexBuffer) {
-        g_pIndexBuffer->Release();
-        g_pIndexBuffer = nullptr;
-    }
-}
-
 
 ATOM App::InitWindowClass(HINSTANCE hInstance) {
     WNDCLASSEXW wcex{};
@@ -123,6 +72,39 @@ BOOL App::InitInstance(HINSTANCE hInstance, int nCmdShow) {
     return TRUE;
 }
 
+DXGI_SWAP_CHAIN_DESC App::CreateSwapChainDesc(HWND hWnd) {
+    DXGI_SWAP_CHAIN_DESC sd{};
+    sd.BufferCount = 2;
+    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    sd.OutputWindow = hWnd;
+    sd.SampleDesc.Count = 1;
+    sd.Windowed = TRUE;
+    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+    return sd;
+}
+
+D3D11_VIEWPORT App::CreateViewport(FLOAT width, FLOAT height) {
+    D3D11_VIEWPORT vp{};
+    vp.TopLeftX = 0.f;
+    vp.TopLeftY = 0.f;
+    vp.Width = (std::max<FLOAT>(width, 1));
+    vp.Height =(std::max<FLOAT>(height, 1));
+    vp.MinDepth = 0.0f;
+    vp.MaxDepth = 1.0f;
+    return vp;
+}
+
+App *App::GetAppFromWndProc(HWND hWnd, UINT msg, LPARAM lParam) {
+    if (msg == WM_NCCREATE) {
+        const auto *pCreate = reinterpret_cast<CREATESTRUCT *>(lParam);
+        auto pApp = static_cast<App *>(pCreate->lpCreateParams);
+        SetWindowLongPtr(hWnd, GWLP_USERDATA, reinterpret_cast<LONG_PTR>(pApp));
+        return pApp;
+    }
+    return reinterpret_cast<App *>(GetWindowLongPtr(hWnd, GWLP_USERDATA));
+}
+
 BOOL App::Init(HINSTANCE hInstance, int nCmdShow) {
     InitWindowClass(hInstance);
     if (!InitInstance(hInstance, nCmdShow)) return FALSE;
@@ -142,28 +124,12 @@ void App::Run() {
             continue;
         }
 
-        if (!pImmediateContext || !g_pRenderTargetView) continue;
+        if (!pImmediateContext || !pRenderTargetView) continue;
 
-        constexpr float clearColor[4] = {0.1f, 0.1f, 0.1f, 1.0f};
-
-        pImmediateContext->ClearRenderTargetView(g_pRenderTargetView, clearColor);
-
-        UINT stride = sizeof(Vertex);
-        UINT offset = 0;
-
-        pImmediateContext->IASetVertexBuffers(0, 1, &g_pVertexBuffer, &stride, &offset);
-        pImmediateContext->IASetInputLayout(g_pVertexLayout);
-        pImmediateContext->IASetPrimitiveTopology(D3D11_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
-        pImmediateContext->VSSetShader(g_pVertexShader, nullptr, 0);
-        pImmediateContext->PSSetShader(g_pPixelShader, nullptr, 0);
-
-        pImmediateContext->IASetIndexBuffer(g_pIndexBuffer, DXGI_FORMAT_R32_UINT, 0);
-        pImmediateContext->DrawIndexed(6, 0, 0);
-
-
+        Draw();
         if (const HRESULT hr = pSwapChain->Present(1, 0); FAILED(hr)) {
             MessageBoxW(nullptr, L"Error presenting swap chain", L"Error", MB_ICONERROR);
-            continue;
+            break;
         }
 
         frameCount++;
@@ -182,15 +148,24 @@ void App::Run() {
     }
 }
 
+void App::Draw() {
+    constexpr FLOAT clearColor[4] = {0.1f, 0.1f, 0.1f, 1.0f};
+
+    pImmediateContext->ClearRenderTargetView(pRenderTargetView.Get(), clearColor);
+
+    pImmediateContext->OMSetRenderTargets(1, pRenderTargetView.GetAddressOf(), nullptr);
+    basicEffect->Apply(pImmediateContext.Get());
+    pImmediateContext->IASetInputLayout(inputLayout.Get());
+
+    spriteBatch->Begin();
+
+    spriteBatch->Draw(spriteTexture.Get(), XMFLOAT2(100, 100));
+
+    spriteBatch->End();
+}
+
 BOOL App::InitDirect3D(HWND hWnd) {
-    DXGI_SWAP_CHAIN_DESC sd{};
-    sd.BufferCount = 2;
-    sd.BufferDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
-    sd.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
-    sd.OutputWindow = hWnd;
-    sd.SampleDesc.Count = 1;
-    sd.Windowed = TRUE;
-    sd.SwapEffect = DXGI_SWAP_EFFECT_DISCARD;
+    const DXGI_SWAP_CHAIN_DESC sd = CreateSwapChainDesc(hWnd);
 
     constexpr D3D_FEATURE_LEVEL levels[] = {
         D3D_FEATURE_LEVEL_11_1,
@@ -199,7 +174,6 @@ BOOL App::InitDirect3D(HWND hWnd) {
         D3D_FEATURE_LEVEL_10_0
     };
     D3D_FEATURE_LEVEL obtained{};
-
 
     UINT flags = 0;
 #if defined(_DEBUG)
@@ -225,124 +199,40 @@ BOOL App::InitDirect3D(HWND hWnd) {
     // Set viewport
     RECT rc{};
     GetClientRect(hWnd, &rc);
-    D3D11_VIEWPORT vp{};
-    vp.TopLeftX = 0.f;
-    vp.TopLeftY = 0.f;
-    vp.Width = static_cast<FLOAT>(std::max<LONG>(rc.right - rc.left, 1));
-    vp.Height = static_cast<FLOAT>(std::max<LONG>(rc.bottom - rc.top, 1));
-    vp.MinDepth = 0.0f;
-    vp.MaxDepth = 1.0f;
+    const FLOAT Width = static_cast<FLOAT>(std::max<LONG>(rc.right - rc.left, 1));
+    const FLOAT Height = static_cast<FLOAT>(std::max<LONG>(rc.bottom - rc.top, 1));
+
+    const D3D11_VIEWPORT vp = CreateViewport(Width, Height);
     pImmediateContext->RSSetViewports(1, &vp);
 
-    // Create vertex and index buffers
-    constexpr Vertex vertices[] = {
-        {{-0.5f, 0.5f, 0.0f}, {0.f, 1.f, 1.f, 1.f}},
-        {{0.5f, 0.5f, 0.0f}, {1.f, 0.f, 1.f, 1.f}},
-        {{0.5f, -0.5f, 0.0f}, {1.f, 1.f, 0.f, 1.f}},
-        {{-0.5f, -0.5f, 0.0f}, {0.f, 0.f, 1.f, 1.f}},
-    };
+    states = std::make_unique<CommonStates>(pd3dDevice.Get());
 
-    constexpr UINT indices[] = {
-        0, 1, 2,
-        0, 2, 3
-    };
+    basicEffect = std::make_unique<BasicEffect>(pd3dDevice.Get());
+    basicEffect->SetVertexColorEnabled(true);
 
-    // Index Buffer
-    D3D11_BUFFER_DESC ibd{};
-    ibd.Usage = D3D11_USAGE_DEFAULT;
-    ibd.ByteWidth = sizeof(indices);
-    ibd.BindFlags = D3D11_BIND_INDEX_BUFFER;
+    void const *shaderByteCode;
+    size_t byteCodeLength;
+    basicEffect->GetVertexShaderBytecode(&shaderByteCode, &byteCodeLength);
 
-    D3D11_SUBRESOURCE_DATA iinitData{};
-    iinitData.pSysMem = indices;
+    pd3dDevice->CreateInputLayout(VertexPositionColor::InputElements,
+                                  VertexPositionColor::InputElementCount,
+                                  shaderByteCode, byteCodeLength,
+                                  &inputLayout);
 
-    hr = pd3dDevice->CreateBuffer(&ibd, &iinitData, &g_pIndexBuffer);
-    if (FAILED(hr)) {
-        _com_error err(hr);
-        MessageBoxW(nullptr, err.ErrorMessage(), L"Failed to create index buffer", MB_ICONERROR);
-        return false;
-    }
+    spriteBatch = std::make_unique<DirectX::SpriteBatch>(pImmediateContext.Get());
 
-    // Vertex Buffer
-    D3D11_BUFFER_DESC bd{};
-    bd.Usage = D3D11_USAGE_DEFAULT;
-    bd.ByteWidth = sizeof(vertices);
-    bd.BindFlags = D3D11_BIND_VERTEX_BUFFER;
-
-    D3D11_SUBRESOURCE_DATA initData{};
-    initData.pSysMem = vertices;
-
-    hr = pd3dDevice->CreateBuffer(&bd, &initData, &g_pVertexBuffer);
-    if (FAILED(hr)) {
-        _com_error err(hr);
-        MessageBoxW(nullptr, err.ErrorMessage(), L"Failed to create vertex buffer", MB_ICONERROR);
-        return false;
-    }
-
-    // Compile and create shaders
-    ID3DBlob* vsBlob = nullptr;
-    ID3DBlob* psBlob = nullptr;
-    ID3DBlob* error = nullptr;
-
-    // Vertex Shader
-    hr = D3DCompileFromFile(L"resources/shader.hlsl", nullptr, nullptr, "VS", "vs_5_0", 0, 0, &vsBlob, &error);
-    if (FAILED(hr)) {
-        if (error) {
-            MessageBoxA(nullptr, static_cast<char*>(error->GetBufferPointer()), "Vertex Shader Compilation Error", MB_ICONERROR);
-            error->Release();
-        } else {
-            MessageBoxA(nullptr, "Unknown error during vertex shader compilation.", "Vertex Shader Compilation Error", MB_ICONERROR);
-        }
-        return false;
-    }
-
-    hr = pd3dDevice->CreateVertexShader(vsBlob->GetBufferPointer(), vsBlob->GetBufferSize(), nullptr, &g_pVertexShader);
-    if (FAILED(hr)) {
-        _com_error err(hr);
-        MessageBoxW(nullptr, err.ErrorMessage(), L"Failed to create vertex shader", MB_ICONERROR);
-        vsBlob->Release();
-        return false;
-    }
-
-    // Input Layout
-    D3D11_INPUT_ELEMENT_DESC layout[] = {
-        {"POSITION", 0, DXGI_FORMAT_R32G32B32_FLOAT, 0, 0, D3D11_INPUT_PER_VERTEX_DATA, 0},
-        {"COLOR", 0, DXGI_FORMAT_R32G32B32A32_FLOAT, 0, 12, D3D11_INPUT_PER_VERTEX_DATA, 0}
-    };
-
-    hr = pd3dDevice->CreateInputLayout(layout, _countof(layout),
-        vsBlob->GetBufferPointer(),
-        vsBlob->GetBufferSize(),
-        &g_pVertexLayout);
-
-    vsBlob->Release(); // Always release blobs after use
+    hr = CreateWICTextureFromFile(
+        pd3dDevice.Get(),
+        pImmediateContext.Get(),
+        L"resources/sprite.jpg",
+        nullptr,
+        &spriteTexture
+    );
 
     if (FAILED(hr)) {
-        MessageBoxA(nullptr, "Failed to create input layout", "Input Layout Error", MB_ICONERROR);
-        return false;
+        MessageBoxW(nullptr, L"Failed to load texture", L"Erro", MB_ICONERROR);
+        return FALSE;
     }
-
-    // Pixel Shader
-    hr = D3DCompileFromFile(L"resources/shader.hlsl", nullptr, nullptr, "PS", "ps_5_0", 0, 0, &psBlob, &error);
-    if (FAILED(hr)) {
-        if (error) {
-            MessageBoxA(nullptr, static_cast<char*>(error->GetBufferPointer()), "Pixel Shader Compilation Error", MB_ICONERROR);
-            error->Release();
-        } else {
-            MessageBoxA(nullptr, "Unknown error during pixel shader compilation.", "Pixel Shader Error", MB_ICONERROR);
-        }
-        return false;
-    }
-
-    hr = pd3dDevice->CreatePixelShader(psBlob->GetBufferPointer(), psBlob->GetBufferSize(), nullptr, &g_pPixelShader);
-    psBlob->Release(); // Release after use
-
-    if (FAILED(hr)) {
-        _com_error err(hr);
-        MessageBoxW(nullptr, err.ErrorMessage(), L"Failed to create pixel shader", MB_ICONERROR);
-        return false;
-    }
-
     return true;
 }
 
@@ -352,11 +242,11 @@ BOOL App::CreateRenderTarget() {
     HRESULT hr = pSwapChain->GetBuffer(0, __uuidof(ID3D11Texture2D), reinterpret_cast<void **>(&pBackBuffer));
     if (FAILED(hr) || !pBackBuffer) return false;
 
-    hr = pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &g_pRenderTargetView);
+    hr = pd3dDevice->CreateRenderTargetView(pBackBuffer, nullptr, &pRenderTargetView);
     pBackBuffer->Release();
     if (FAILED(hr)) return false;
+    pImmediateContext->OMSetRenderTargets(1, pRenderTargetView.GetAddressOf(), nullptr);
 
-    pImmediateContext->OMSetRenderTargets(1, &g_pRenderTargetView, nullptr);
     return true;
 }
 
@@ -364,23 +254,15 @@ void App::Resize(const UINT width, const UINT height) {
     if (!pSwapChain) return;
 
     pImmediateContext->OMSetRenderTargets(0, nullptr, nullptr);
-    if (g_pRenderTargetView) {
-        g_pRenderTargetView->Release();
-        g_pRenderTargetView = nullptr;
-    }
+    pRenderTargetView.Reset();
 
     if (const HRESULT hr = pSwapChain->ResizeBuffers(0, width, height, DXGI_FORMAT_UNKNOWN, 0); FAILED(hr)) {
         const _com_error err(hr);
-        MessageBoxW(nullptr,err.ErrorMessage(),L"Failed to resize swap chain",MB_ICONERROR);
+        MessageBoxW(nullptr, err.ErrorMessage(), L"Failed to resize swap chain",MB_ICONERROR);
     }
     CreateRenderTarget();
 
-    D3D11_VIEWPORT vp{};
-    vp.TopLeftX = 0;
-    vp.TopLeftY = 0;
-    vp.Width = static_cast<FLOAT>(max(width, 1u));
-    vp.Height = static_cast<FLOAT>(max(height, 1u));
-    vp.MinDepth = 0.0f;
-    vp.MaxDepth = 1.0f;
+    // Set viewport
+    const D3D11_VIEWPORT vp = CreateViewport(static_cast<FLOAT>(width), static_cast<FLOAT>(height));
     pImmediateContext->RSSetViewports(1, &vp);
 }
--- src/app.h
+++ src/app.h
@@ -7,42 +7,46 @@
 
 #include <windows.h>
 #include <d3d11.h>
-#include <iostream>
+#include <wrl/client.h>
+#include <CommonStates.h>
+#include <Effects.h>
+#include <SpriteBatch.h>
 
+using Microsoft::WRL::ComPtr;
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT msg, WPARAM wParam, LPARAM lParam);
 
 class App {
-    struct Vertex {
-        float position[3];
-        float color[4];
-    };
 
 public:
     App();
 
-    ~App();
+    ~App()=default;
 
     BOOL Init(HINSTANCE hInstance, int nCmdShow);
-    BOOL bIsRunning = TRUE;
-    void Run();
-    void Resize(UINT width, UINT height);
 
+    BOOL bIsRunning {TRUE};
+
+    void Run();
+    void Draw();
+    void Resize(UINT width, UINT height);
+    static App* GetAppFromWndProc(HWND hWnd, UINT msg, LPARAM lParam);
 private:
     HINSTANCE hInst{};
     WCHAR szTitle[100]{};
     WCHAR szWindowClass[100]{};
 
-    ID3D11Device *pd3dDevice = nullptr;
-    ID3D11DeviceContext *pImmediateContext = nullptr;
-    IDXGISwapChain *pSwapChain = nullptr;
-    ID3D11RenderTargetView *g_pRenderTargetView = nullptr;
+    ComPtr<ID3D11Device> pd3dDevice = nullptr;
+    ComPtr<ID3D11DeviceContext> pImmediateContext = nullptr;
+    ComPtr<IDXGISwapChain> pSwapChain = nullptr;
+    ComPtr<ID3D11RenderTargetView> pRenderTargetView = nullptr;
 
-    ID3D11Buffer *g_pVertexBuffer = nullptr;
-    ID3D11InputLayout *g_pVertexLayout = nullptr;
-    ID3D11VertexShader *g_pVertexShader = nullptr;
-    ID3D11PixelShader *g_pPixelShader = nullptr;
-    ID3D11Buffer *g_pIndexBuffer = nullptr;
+    // DirectXTK helpers
+    std::unique_ptr<DirectX::CommonStates> states;
+    std::unique_ptr<DirectX::BasicEffect> basicEffect;
+    ComPtr<ID3D11InputLayout> inputLayout;
+    std::unique_ptr<DirectX::SpriteBatch> spriteBatch;
+    ComPtr<ID3D11ShaderResourceView> spriteTexture;
 
     ATOM InitWindowClass(HINSTANCE hInstance);
 
@@ -51,8 +55,10 @@ private:
     BOOL CreateRenderTarget();
 
     BOOL InitInstance(HINSTANCE hInstance, int nCmdShow);
-};
+    static DXGI_SWAP_CHAIN_DESC CreateSwapChainDesc(HWND hWnd);
+    static D3D11_VIEWPORT CreateViewport(FLOAT width, FLOAT height);
 
+};
 
 
 #endif //APP_H
